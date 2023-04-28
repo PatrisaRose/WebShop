e0dcc28c1abaaafade8b8e2f1b5019388bfd551f
PatrsiaRose <cseresznyevirag4@gmail.com>
2023-04-28 21:45:09 +0200

    táblázat

--- adatkezeles.js
+++ adatkezeles.js
@@ -1,13 +1,17 @@
+let aside = $("aside");
+
 export function osszeallitPc(adatLista, kulcsLista) {
   let txt = "";
   txt += `<div class = "container">`;
   for (let index = 0; index < adatLista.length; index++) {
     txt += `<div class= "kartya">`;
     for (const kulcs in kulcsLista[0]) {
-      if (kulcs != "termekKepe") {
+      if (kulcs != "termekKepe" && kulcs != "ar") {
         if (adatLista[index][kulcs] != "") {
           txt += `<p>${kulcsLista[0][kulcs]}: ${adatLista[index][kulcs]}</p>`;
         }
+      } else if (kulcs === "ar") {
+        txt += `${adatLista[index][kulcs]} Ft.-`;
       } else {
         txt += "<img src='" + adatLista[index][kulcs] + "' alt=''>";
       }
@@ -20,20 +24,58 @@ export function osszeallitPc(adatLista, kulcsLista) {
 
 export function osszeallitTablazat(adatLista, kulcsLista) {
   let txt = "";
+  txt += `<div>`;
+  txt += `<label for="szures">Név szűrő:</label>`;
+  txt += `<input type="text" id="szures" name="szures"/>`;
+  txt += `<button id="szurogomb">Szűrés</button>`;
+  txt += `</div>`;
   txt += `<table>`;
+  txt += `<tr>`;
+  for (const kulcs in kulcsLista[0]) {
+    if (kulcs != "termekKepe") {
+      txt += `<th id=${kulcs}>${kulcsLista[0][kulcs]}</th>`;
+    } else {
+      txt += `<th id=${kulcs}>Termék képe</th>`;
+    }
+  }
+  txt += `</tr>`;
   for (let index = 0; index < adatLista.length; index++) {
-    txt += `<tr>`
+    txt += `<tr>`;
     for (const kulcs in kulcsLista[0]) {
-      if (kulcs != "termekKepe") {
+      if (kulcs != "termekKepe" && kulcs != "ar") {
         if (adatLista[index][kulcs] != "") {
-          txt += `<td>${kulcsLista[0][kulcs]}: ${adatLista[index][kulcs]}</td>`
+          txt += `<td>${adatLista[index][kulcs]}</td>`;
+        } else {
+          txt += `<td>Nincs adat</td>`;
         }
+      } else if (kulcs === "ar") {
+        txt += `<td>${adatLista[index][kulcs]} Ft.-</td>`;
       } else {
         txt += "<td><img src='" + adatLista[index][kulcs] + "' alt=''></td>";
       }
     }
-    txt += `</tr>`
+    txt += `</tr>`;
   }
   txt += `<table>`;
   return txt;
+}
+
+export function szures(adatLista, kulcsLista) {
+  let katt = 0;
+  let tablaFejlec = $("th:not(#termekKepe)");
+  let lista = adatLista;
+  let eredeti = adatLista;
+  tablaFejlec.click(function () {
+    katt++;
+    if (katt === 0) {
+      lista = eredeti;
+    } else if (katt === 1) {
+      lista.sort((a, b) => a - b);
+    } else if (katt === 2) {
+      lista.sort((b, a) => b - a);
+    } else {
+      katt = 0;
+    }
+    osszeallitTablazat(adatLista, kulcsLista);
+  });
 }
--- felhasznalokKezelese.js
+++ felhasznalokKezelese.js
@@ -1,13 +1,46 @@
 import { adatBeolvas } from "./fetch.js";
-import { osszeallitTablazat } from "./adatkezeles.js";
+import { osszeallitTablazat, szures } from "./adatkezeles.js";
 let PCLista = [];
 let PCKulcsok = [];
+let PCKepek = [
+  "kepek/rogue.png",
+  "kepek/skynetPC.png",
+  "kepek/Ultra_i9_Pro_2.png",
+  "kepek/Ryzen_9_5900X_3.png",
+  "kepek/navon-nex-1401-black_1_m.png",
+  "kepek/Acer_Travelmate.png",
+  "kepek/Lenovo_Ideapad_3_Arctic_Grey.png",
+  "kepek/HP_14S-FQ0036NL_White.png",
+];
 let tartozekLista = [];
 let tartozekKulcsok = [];
+let tartozekKepek = [
+  "kepek/Havit_MS1017.png",
+  "kepek/Acer_Predator_Cestus_330.png",
+  "kepek/Trust_GXT_930_Jacx.png",
+  "kepek/Glorious_Model_D_RGB.png",
+  "kepek/FIFINE_A6V.png",
+  "kepek/SteelSeries_QcK_XXL__67500.png",
+  "kepek/White_Shark_GK-2101HU_SPARTAN-X.png",
+  "kepek/Razer_Blackshark_V2_X.png",
+];
 let konzolKulcsok = [];
 let konzolLista = [];
+let konzolKepek = [
+  "kepek/Sony_PlayStation_5__PS5.png",
+  "kepek/Microsoft_Xbox_Series_X_1TB.png",
+  "kepek/Nintendo_Switch.png",
+  "kepek/BlackBird_BH1368_hordozható_Tetris.png",
+  "kepek/Arcade1up_Capcom_Legacy.png",
+  "kepek/PlayStation_5__PS5__DualSense.png",
+  "kepek/Microsoft_Xbox_kontroller.png",
+  "kepek/Nintendo_Joy-Con_kontroller_par.png",
+];
 let felhasznalok = [];
 let vegpont = "adatok.json";
+let kivalasztas = $(".kivalasztas");
+let tablazat = $(".tablazat");
+let inputokHelye = $(".inputokHelye");
 
 $(function () {
   adatBeolvas(vegpont, listaFeltolt);
@@ -91,39 +124,50 @@ function opciok() {
   txt2 += `<option value="Tartozékok">Tartozékok</option>`;
   txt2 += `</select>`;
   txt2 += `</form>`;
-  aside.html(txt2);
+  kivalasztas.append(txt2);
+  tablazat.html(osszeallitTablazat(PCLista, PCKulcsok));
+  szures(PCLista, PCKulcsok);
   $("select").change(function (oldal) {
     oldal.preventDefault();
     let txt3 = "";
     let valasztas = $(".tipusok :selected").text();
     if (valasztas === "Számítógép") {
-      txt3 += `<form>`;
+      tablazat.html(osszeallitTablazat(PCLista, PCKulcsok));
+      szures(PCLista, PCKulcsok);
+      txt3 += `<form class="kuldes">`;
       txt3 += `<fieldset class="adatInput" >`;
       txt3 += `<legend>Új Termék felvétele:</legend>`;
 
       for (let i = 0; i < PCKulcsok.length; i++) {
         for (const key in PCKulcsok[i]) {
-          if (key !== "termekKepe") {
-            console.log(i);
+          if (key !== "termekKepe" && key !== "ar") {
             txt3 += `<div>`;
             txt3 += `<label for="${key}">` + PCKulcsok[i][key] + `:</label>`;
-            txt3 += `<input type="text" id="${key}" name="${key}"/>`;
+            txt3 += `<input pattern="[\\w]+" type="text" id="${key}" name="${key}"/>`;
+            txt3 += `</div>`;
+          } else if (key === "ar") {
+            txt3 += `<div>`;
+            txt3 += `<label for="${key}">` + PCKulcsok[i][key] + `:</label>`;
+            txt3 += `<input pattern="[\\d]+" type="text" id="${key}" name="${key}"/>`;
+            txt3 += `</div>`;
+          } else {
+            txt3 += `<div>`;
+            txt3 += `<label for="tkepe">Termék Képe:</label>`;
+            txt3 += `<input value=${
+              PCKepek[Math.floor(Math.random() * PCKepek.length)]
+            } id="${key}" name="${key}>`;
             txt3 += `</div>`;
           }
         }
       }
 
-      txt3 += `<div>`;
-      txt3 += `<label for="tkepe">Termék Képe:</label>`;
-      txt3 += `<input type="file" id="avatar" name="avatar" accept="image/png, image/jpeg">`;
-      txt3 += `</div>`;
       txt3 += `</fieldset>`;
-      txt3 += `<button class="kuldes">Küldés</button>`;
+      txt3 += `<button>Küldés</button>`;
       txt3 += `</form>`;
 
-      aside.html(txt3);
+      inputokHelye.html(txt3);
 
-      $(".kuldes").on("click", function (oldal) {
+      $(".kuldes").submit(function (oldal) {
         oldal.preventDefault();
         let termek = {};
         const ADAT = $(".adatInput input");
@@ -133,38 +177,46 @@ function opciok() {
           i++;
         }
         PCLista.push(termek);
-        aside.append(osszeallitTablazat(PCLista, PCKulcsok));
+        tablazat.html(osszeallitTablazat(PCLista, PCKulcsok));
       });
     } else if (valasztas === "Konzol") {
-      console.log($("select option"));
-      txt3 += `<form>`;
+      tablazat.html(osszeallitTablazat(konzolLista, konzolKulcsok));
+      szures(konzolLista, konzolKulcsok);
+      txt3 += `<form class="kuldes">`;
       txt3 += `<fieldset class="adatInput">`;
       txt3 += `<legend>Új Termék felvétele:</legend>`;
 
       for (let i = 0; i < konzolKulcsok.length; i++) {
         for (const key in konzolKulcsok[i]) {
-          if (key !== "termekKepe") {
-            console.log(i);
+          if (key !== "termekKepe" && key !== "ar") {
             txt3 += `<div>`;
-            txt3 += `<label for="${key}">` + konzolKulcsok[i][key] + `:</label>`;
-            txt3 += `<input type="text" id="${key}" name="${key}"/>`;
+            txt3 +=
+              `<label for="${key}">` + konzolKulcsok[i][key] + `:</label>`;
+            txt3 += `<input pattern="[\\w]+" type="text" id="${key}" name="${key}"/>`;
+            txt3 += `</div>`;
+          } else if (key === "ar") {
+            txt3 += `<div>`;
+            txt3 +=
+              `<label for="${key}">` + konzolKulcsok[i][key] + `:</label>`;
+            txt3 += `<input pattern="[\\d]+" type="text" id="${key}" name="${key}"/>`;
+            txt3 += `</div>`;
+          } else {
+            txt3 += `<div>`;
+            txt3 += `<label for="tkepe">Termék Képe:</label>`;
+            txt3 += `<input value=${
+              konzolKepek[Math.floor(Math.random() * konzolKepek.length)]
+            }>`;
             txt3 += `</div>`;
           }
         }
       }
 
-      txt3 += `<div>`;
-      txt3 += `<label for="tkepe">Termék Képe:</label>`;
-      txt3 += `<input type="file"
-      id="avatar" name="avatar"
-      accept="image/png, image/jpeg">`;
-      txt3 += `</div>`;
       txt3 += `</fieldset>`;
-      txt3 += `<button class="kuldes" >Küldés</button>`;
+      txt3 += `<button>Küldés</button>`;
       txt3 += `</form>`;
-      aside.html(txt2);
-      aside.html(txt3);
-      $(".kuldes").on("click", function (oldal) {
+
+      inputokHelye.html(txt3);
+      $(".kuldes").submit(function (oldal) {
         oldal.preventDefault();
         let termek = {};
         const ADAT = $(".adatInput input");
@@ -174,47 +226,56 @@ function opciok() {
           i++;
         }
         tartozekLista.push(termek);
-        aside.append(osszeallitTablazat(tartozekLista, tartozekKulcsok));
+        tablazat.html(osszeallitTablazat(konzolLista, konzolKulcsok));
       });
     } else if (valasztas === "Tartozékok") {
-      txt3 += `<form>`;
+      tablazat.html(osszeallitTablazat(tartozekLista, tartozekKulcsok));
+      szures(tartozekLista, tartozekKulcsok)
+      txt3 += `<form class="kuldes">`;
       txt3 += `<fieldset class="adatInput">`;
       txt3 += `<legend>Új Termék felvétele:</legend>`;
 
       for (let i = 0; i < tartozekKulcsok.length; i++) {
         for (const key in tartozekKulcsok[i]) {
-          if (key !== "termekKepe") {
+          if (key !== "termekKepe" && key !== "ar") {
             console.log(i);
             txt3 += `<div>`;
-            txt3 += `<label for="${key}">` + tartozekKulcsok[i][key] + `:</label>`;
+            txt3 +=
+              `<label for="${key}">` + tartozekKulcsok[i][key] + `:</label>`;
             txt3 += `<input type="text" id="${key}" name="${key}"/>`;
+            txt3 += `</div>`;
+          } else if (key === "ar") {
+            txt3 += `<div>`;
+            txt3 +=
+              `<label for="${key}">` + tartozekKulcsok[i][key] + `:</label>`;
+            txt3 += `<input pattern="[\\d]+" type="text" id="${key}" name="${key}"/>`;
+            txt3 += `</div>`;
+          } else {
+            txt3 += `<div>`;
+            txt3 += `<label for="tkepe">Termék Képe:</label>`;
+            let szam = Math.floor(Math.random() * tartozekKepek.length);
+            txt3 += `<input value=${tartozekKepek[szam]}>`;
             txt3 += `</div>`;
           }
         }
       }
 
-      txt3 += `<div>`;
-      txt3 += `<label for="tkepe">Termék Képe:</label>`;
-      txt3 += `<input type="file"
-      id="avatar" name="avatar"
-      accept="image/png, image/jpeg">`;
-      txt3 += `</div>`;
       txt3 += `</fieldset>`;
-      txt3 += `<button class="kuldes" >Küldés</button>`;
+      txt3 += `<button>Küldés</button>`;
       txt3 += `</form>`;
-      aside.html(txt2);
-      aside.html(txt3);
-      $(".kuldes").on("click", function (oldal) {
+
+      inputokHelye.html(txt3);
+      $(".kuldes").submit(function (oldal) {
         oldal.preventDefault();
         let termek = {};
         const ADAT = $(".adatInput input");
         let i = 0;
-        for (const kulcs in konzolLista[i]) {
-          termek[kulcs] = `${ADAT[i].value}`;
+        for (const kulcs in tartozekLista[i]) {
+          termek[kulcs] = $(ADAT.eq(i)).val();
           i++;
         }
-        konzolLista.push(termek);
-        aside.append(osszeallitTablazat(konzolLista, konzolKulcsok));
+        tartozekLista.push(termek);
+        tablazat.html(osszeallitTablazat(tartozekLista, tartozekKulcsok));
       });
     }
   });
